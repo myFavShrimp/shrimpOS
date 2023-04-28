8494c06366868a922b16b67022df05c83e9c5ae4
myFavShrimp <myfavshrimp@tuta.io>
2023-04-28 22:00:31 +0200

    Added alacritty, helix and starship

--- build.sh
+++ build.sh
@@ -1,11 +1,26 @@
 #!/bin/bash
-# remove the default firefox (from fedora) in favor of the flatpak
-rpm-ostree override remove firefox firefox-langpacks
-
 echo "-- Installing RPMs defined in recipe.yml --"
 rpm_packages=$(yq '.rpms[]' < /tmp/shrimpos-recipe.yml)
 for pkg in $(echo -e "$rpm_packages"); do \
     echo "Installing: ${pkg}" && \
     rpm-ostree install $pkg; \
 done
+echo "---"
+
+releasever="$(rpm -E %fedora)"
+basearch="$(rpm -E %_arch)"
+
+# TODO: add repo url && fix `ostree remote add` command
+echo "--- Install packages ---"
+
+echo "-- Install Alacritty --"
+ostree remote add atim/alacritty
+rpm-ostree install alacritty
+
+echo "-- Install helix --"
+ostree remote add helix-editor https://download.copr.fedorainfracloud.org/results/varlad/helix/fedora-$releasever-$basearch/
+rpm-ostree install helix
+
+echo "-- Install starship --"
+curl -sS https://starship.rs/install.sh 
 echo "---"
