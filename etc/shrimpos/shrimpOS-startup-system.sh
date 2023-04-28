a5164250d044af8d64c9f0cd62153f5d99a27b35
myFavShrimp <myfavshrimp@tuta.io>
2023-04-28 22:03:03 +0200

    Fixed typo

--- etc/shrimpos/shrimpOS-startup-system.sh
+++ etc/shrimpos/shrimpOS-startup-system.sh
@@ -1,8 +1,1 @@
-# if test "$(id -u)" -gt "0" && test -d "$HOME"; then
-#     if test ! -e "$HOME"/.config/autostart/shrimpOS-startup-system.desktop; then
-#         mkdir -p "$HOME"/.config/autostart
-#         cp -f /etc/skel.d/.config/autostart/shrimpOS-startup-system.desktop "$HOME"/.config/autostart
-#     fi
-# fi
-
 just -f /etc/shrimpos/justfile setup-flatpaks
