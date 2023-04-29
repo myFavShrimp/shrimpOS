ed0e3b3caa22b550644e5032732923fe36626f96
myFavShrimp <myfavshrimp@tuta.io>
2023-04-29 11:57:35 +0200

    Changed variable case

--- build.sh
+++ build.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
-releasever="$(rpm -E %fedora)"
-basearch="$(rpm -E %_arch)"
+RELEASEVER="$(rpm -E %fedora)"
+BASEARCH="$(rpm -E %_arch)"
 
 # TODO: add repo url && fix `ostree remote add` command
 echo "--- Install packages ---"
 
 echo "-- Install Alacritty --"
-ostree remote add alacritty https://download.copr.fedorainfracloud.org/results/atim/alacritty/fedora-$releasever-$basearch/
+ostree remote add alacritty https://download.copr.fedorainfracloud.org/results/atim/alacritty/fedora-$RELEASEVER-$BASEARCH/
 rpm-ostree install alacritty
 
 echo "-- Install helix --"
-ostree remote add helix-editor https://download.copr.fedorainfracloud.org/results/varlad/helix/fedora-$releasever-$basearch/
+# use fedora 38 helix as no package is available for fedora 39
+HELIX_RELEASEVER=38
+ostree remote add helix-editor https://download.copr.fedorainfracloud.org/results/varlad/helix/fedora-$HELIX_RELEASEVER-$BASEARCH/
 rpm-ostree install helix
 
 echo "-- Install starship --"
