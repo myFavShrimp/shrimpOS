8111b9822c7ddadf10525a852545d75a6942de14
myFavShrimp <myfavshrimp@tuta.io>
2023-04-30 18:39:46 +0200

    Added installation of settings from myFavShrimp/cfg

--- build.sh
+++ build.sh
@@ -10,15 +10,24 @@ echo "--- Install packages ---"
 # shrimpos
 echo "-- Install shrimpos --"
 rpm-ostree install /var/shrimpos.rpm -y --force-replacefiles
-
 # coprs
 echo "-- Install Alacritty --"
 rpm-ostree install alacritty -y
-
 echo "-- Install helix --"
 rpm-ostree install helix -y
-
 echo "---"
 
-echo "--- Updating dconf databases---"
+echo "--- Cloning myFavShrimp/cfg ---"
+cd /tmp
+git clone https://github.com/myFavShrimp/cfg.git
+cd cfg
+echo "--- Installing default gnome settings ---"
+mkdir -p /usr/etc/dconf/db/local.d
+cp gnome/* /usr/etc/dconf/db/local.d/
+echo "--- Installing extensions ---"
+make extensions
+echo "--- Installing flatpaks ---"
+make flatpaks
+echo "--- Updating dconf databases ---"
 dconf update
+echo "---"
